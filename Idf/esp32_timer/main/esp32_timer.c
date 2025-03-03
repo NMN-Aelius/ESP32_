55a8107dffcf4ee8ab94d9a9a550d20209354d0f
Aelius <shineforworks@gmail.com>
2025-03-03 21:01:46 +0700

    [REMOVE] Remove not essential file

--- Idf/esp32_timer/main/esp32_timer.c
+++ Idf/esp32_timer/main/esp32_timer.c
@@ -24,19 +24,23 @@ void t1_callback_fcn(void* argu) {
     ESP_LOGI(tag_timer, "Trong argument-config: Age = %d, Name = %s", ptrcallback->age, ptrcallback->name);
 }
 
-// Biến toàn cục để giữ handle của timer
+// Global variable for hold periodic timer
 esp_timer_handle_t periodic_timer;
+// Global variable for hold one-shot timer
+esp_timer_handle_t one_timer;
 
-// Hàm tạo timer
-int config_timer(void* ptr_arg, char* ptr_name, esp_timer_handle_t* timer_handle) {
-    esp_timer_create_args_t timer1 = {
+// Function to configurer timer
+int config_timer(void* ptr_arg, char* ptr_name, esp_timer_handle_t* timer_handle, esp_timer_create_args_t timer) {
+    esp_timer_create_args_t cfg_timer = {
         .callback = t1_callback_fcn,
         .dispatch_method = ESP_TIMER_TASK,
         .arg = ptr_arg,
-        .name = ptr_names
+        .name = ptr_name
     };
+    
+    timer = cfg_timer;
 
-    return esp_timer_create(&timer1, timer_handle);
+    return esp_timer_create(&timer, timer_handle);
 }
 
 // app_main
@@ -46,25 +50,42 @@ void app_main(void) {
 
     // allocation for newptr to handle data type in struct.
     argument_config* newptr = (argument_config*)malloc(sizeof(argument_config));
-    if (newptr == NULL) {
+    argument_config* newptr1 = (argument_config*)malloc(sizeof(argument_config));
+    if (newptr == NULL || newptr1 == NULL) {
         ESP_LOGE(tag_mmc, "Failed to allocate memory!");
         return;
     }
 
     // assign new value
-    newptr->age = 42;
+    newptr->age = 10;
+    newptr1->age = 11;
     //due to it a string, so must be add by command strcpy instead of normal assign
     strcpy(newptr->name, "nhut");
+    strcpy(newptr1->name, "nhut2");
 
-    // create esp_timer
-    esp_err_t err = config_timer(newptr, newptr->name, &periodic_timer);
+    // create periodic esp_timer
+    esp_timer_create_args_t timer_per;
+    esp_err_t err = config_timer(newptr, newptr->name, &periodic_timer, timer_per);
     if (err != ESP_OK) {
-        ESP_LOGE(tag_timer, "Timer fail!");
-        free(newptr);  // Giải phóng bộ nhớ nếu tạo thất bại
+        ESP_LOGE(tag_timer, "Timer PERIODIC fail!");
+        free(newptr);
         return;
     }
 
     // timer start
     ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, 500000));
-    ESP_LOGI(tag_timer, "Timer created and started successfully!");
+    ESP_LOGI(tag_timer, "Timer PERIODIC created and started successfully!");
+
+    // create one-shot esp_timer
+    esp_timer_create_args_t timer_one;
+    esp_err_t err1 = config_timer(newptr1, newptr1->name, &one_timer, timer_one);
+    if (err1 != ESP_OK) {
+        ESP_LOGE(tag_timer, "Timer ONE SHOT fail!");
+        free(newptr1);
+        return;
+    }
+
+    // timer start
+    ESP_ERROR_CHECK(esp_timer_start_once(one_timer, 3000000));
+    ESP_LOGI(tag_timer, "Timer ONE SHOT created and started successfully!");
 }
